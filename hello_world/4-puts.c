876391b66a041ae5f00daee116dc89e3efefe00f
timpla1 <plancasstim@gmail.com>
2025-10-22 19:07:03 +0200

    rm a " "

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	char* str1 = "\"Programming is like building a multilingual puzzl\n";
+	char* str1 ="\"Programming is like building a multilingual puzzle\n";
 	puts(str1);
 
 	return(0);
