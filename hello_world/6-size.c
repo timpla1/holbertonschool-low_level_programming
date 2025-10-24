b8cd9dbd3271e4e15090bfcd8e738b61ce1bb91b
timpla1 <plancasstim@gmail.com>
2025-10-24 10:36:05 +0200

    rm a "s"

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -15,10 +15,10 @@ int main (void)
 	float e;
 
 	printf("Size of a char: %d byte(s)\n", (int)sizeof(b));
-	printf("Size of an int: %d bytes(s)\n", (int)sizeof(a));
+	printf("Size of an int: %d byte(s)\n", (int)sizeof(a));
 	printf("Size of a long int: %d byte(s)\n", (int)sizeof(c));
 	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(d));
-	printf("Size of a float: %d byte(s)", (int)sizeof(e));
+	printf("Size of a float: %d byte(s)\n", (int)sizeof(e));
 
 	return (0);
 }
