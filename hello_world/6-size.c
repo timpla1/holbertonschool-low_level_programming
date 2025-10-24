f5de90a9f75d3744cd4f445a46b43393962f4237
timpla1 <plancasstim@gmail.com>
2025-10-24 10:16:05 +0200

    6-size.c : printf sizes

---
+++ hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	int a;
+	char b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %d byte(s)\n", (int)sizeof(b));
+	printf("Size of an int: %d bytes(s)\n", (int)sizeof(a));
+	printf("Size of a long int: %d byte(s)\n", (int)sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(d));
+	printf("Size of a float: %d byte(s)\n", (int)sizeof(e));
+
+	return (0);
+}
