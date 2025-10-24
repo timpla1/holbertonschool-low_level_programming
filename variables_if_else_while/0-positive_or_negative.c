05e4b2d9ae0bd08bde48b9c9bd07cc1795085d71
timpla1 <plancasstim@gmail.com>
2025-10-24 17:11:38 +0200

    betty correction

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -17,17 +17,17 @@ int main(void)
 
 	if (n < 0)
 {
-	printf("%d is negative\n",n);
+	printf("%d is negative\n", n);
 }
 
 	if (n > 0)
 {
-	printf("%d is positive\n",n);
+	printf("%d is positive\n", n);
 }
 
 	if (n == 0)
 {
-	printf("%d is zero\n",n);
+	printf("%d is zero\n", n);
 }
 
 	return (0);
