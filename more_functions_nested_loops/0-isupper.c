504078f6030ddfe8d2668524d0e51fc7cfe60368
timpla1 <plancasstim@gmail.com>
2025-10-28 10:42:41 +0100

    0.isupper. if returns(1/0)

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+
+	if (c >= 'a' && c <= 'z')
+	{
+	return (0);
+	}
+	return (0);
+}
