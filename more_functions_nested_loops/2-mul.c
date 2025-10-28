3c2fd56d587d64c49eb59e9a5df8134495675de6
timpla1 <plancasstim@gmail.com>
2025-10-28 14:29:50 +0100

    2-mul.c return (a * b)

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - prevent from possible conflic in my calls
+ * @a: a number for the multiplication
+ * @b: another number for the multiplication 
+ *
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
