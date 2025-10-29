dcb300a8a82f5bc29c55164622bb67e5c7a9a41f
timpla1 <plancasstim@gmail.com>
2025-10-29 10:45:46 +0100

    4. for and _putchar

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print all the numbers exept 2 and 4
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+		if (c != '2' && c != '4')
+			_putchar(c);
+
+	_putchar('\n');
+}
