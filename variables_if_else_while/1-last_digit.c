db9e79d45ea6906497da8c6eec39f674edaf9b8f
timpla1 <plancasstim@gmail.com>
2025-11-01 19:37:22 +0100

    five if and printf

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	char knee[] = "Last digit of";
+	char he[] = "and is less than 6 and not 0";
+	char E[] = "and is greater than 5";
+	char I[] = "and is 0";
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	printf("%s %d is", knee, n);
+
+	if (n < 0)
+	{
+	n = n * (-1);
+	}
+	if ((n % 10) < 6 && (n % 10) > 0)
+	{
+	printf(" %d %s\n", (n % 10), he);
+	}
+	if ((n % 10) > 5)
+	{
+	printf(" %d %s\n", (n % 10), E);
+	}
+	if ((n % 10) == 0)
+	{
+	printf(" %d %s\n", (n % 10), I);
+	}
+
+	return (0);
+}
