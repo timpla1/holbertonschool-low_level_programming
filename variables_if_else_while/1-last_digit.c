71195930d3ecc641becdde5a7fa8dd0f8e04774c
timpla1 <plancasstim@gmail.com>
2025-11-01 19:48:01 +0100

    negative numbers correction

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,11 +21,7 @@ int main(void)
 
 	printf("%s %d is", knee, n);
 
-	if (n < 0)
-	{
-	n = n * (-1);
-	}
-	if ((n % 10) < 6 && (n % 10) > 0)
+	if ((n % 10) < 6 && (n % 10) != 0)
 	{
 	printf(" %d %s\n", (n % 10), he);
 	}
