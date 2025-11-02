78bc687acbfea6167a746d6721dd7c5bac6bfcf6
timpla1 <plancasstim@gmail.com>
2025-11-02 15:52:38 +0100

    '0' to '9' with putchar

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+	putchar('0' + n);
+	}
+	putchar('\n');
+
+	return (0);
+}
