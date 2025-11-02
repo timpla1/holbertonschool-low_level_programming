2e4adb1ffa8948d445a1ee0cc00624d3cc6e0fec
timpla1 <plancasstim@gmail.com>
2025-11-02 19:29:17 +0100

    alphabet with _putchar(knee)

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+
+	char knee;
+
+	for (knee = 'a'; knee <= 'z'; knee++)
+	{
+	_putchar(knee);
+	}
+	_putchar('\n');
+}
