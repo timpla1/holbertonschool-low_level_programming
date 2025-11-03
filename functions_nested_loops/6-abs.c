86cbf447e4cd8143b9632e8a84f9a93f1cc069a4
timpla1 <plancasstim@gmail.com>
2025-11-03 18:12:15 +0100

    6-abs.c absolute numbers

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -6,13 +6,19 @@
  * Return: Always 0.
  */
 
-int _abs(int)
-{
-	int n;
-	
-
-	n = _abs(-1);
+int _abs(int n)
+{	
+	if (n > 0)
+	{
 	_putchar(n);
-	
+	}
+	if (n < 0)
+	{
+	_putchar(-n);
+	}
+	if (n == 0)
+	{
+	puthcar('0');
+	}
 	return (0);
 }
